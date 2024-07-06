157fff211b3e22e0786d7564bef1c87b0eaaba5b
paul uwaifo <uwaifopaul1@gmail.com>
2024-07-06 20:44:41 +0100

    updated

--- src/components/sections/Section_2.jsx
+++ src/components/sections/Section_2.jsx
@@ -1,31 +1,15 @@
-import axios from "axios";
 import Card from "../utility/Card";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { products } from "../../products";
 
 function Section_2() {
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    const fetch = async () => {
-      axios
-        .get("./public/products.json")
-        .then((res) => {
-          setProducts(res.data.products);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    fetch();
-  }, []);
 
   return (
     <div className="flex wrap flex-col mt-24">
       {/* heading */}
       <div className="flex flex-row">
         <div className="basis-1/2">
-          <p className="text-4xl font-semibold">Top </p>
+          <p className="text-4xl font-semibold">Top products</p>
         </div>
         <div className="basis-1/2 text-[1.2rem] flex justify-end font-semibold">
           <Link className="mx-2" to={"/"}>
---
+++ src/products.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: "GGOEAFKA087499",
+    name: "Wood sandalwood dossier",
+    brand: "channel",
+    description:
+      "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+    dimensions: "7 x 6 inch",
+
+    keywords:
+      "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
+    image: ["./product_images/img_2822_480.jpg", "image2"],
+    price: 98,
+
+    qty: 10,
+  },
+  {
+    id: "GGOEAFKA087499",
+    name: "fragrance enhancer, DS & DURGA",
+    brand: "channel",
+    description:
+      "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+    dimensions: "7 x 6 inch",
+
+    keywords:
+      "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
+    image: ["./product_images/img_2823_480.jpg", "image2"],
+    price: 125,
+
+    qty: 10,
+  },
+  {
+    id: "GGOEAFKA087499",
+    name: "Utredo Blanche",
+    brand: "channel",
+    description:
+      "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+    dimensions: "7 x 6 inch",
+
+    keywords:
+      "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
+    image: ["./product_images/img_2824_480.jpg", "image2"],
+    price: 100,
+
+    qty: 10,
+  },
+  {
+    id: "GGOEAFKA087499",
+    name: "Feminine OUD Adwene",
+    brand: "channel",
+    description:
+      "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+    dimensions: "7 x 6 inch",
+
+    keywords:
+      "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
+    image: ["./product_images/img_2825_480.jpg", "image2"],
+    price: 132,
+
+    qty: 10,
+  },
+];
