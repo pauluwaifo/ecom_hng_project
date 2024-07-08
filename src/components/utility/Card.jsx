60bf2749e4862ebfec93aae88c7b7a9027c36dd4
paul uwaifo <uwaifopaul1@gmail.com>
2024-07-08 16:48:48 +0100

    updated

--- src/components/utility/Card.jsx
+++ src/components/utility/Card.jsx
@@ -2,7 +2,7 @@ import { IoStar, IoStarHalf } from "react-icons/io5";
 import { useContext, useState } from "react";
 import AppContext from "../../context/AppContext";
 
-function Card({ item }) {
+function Card({ item, url }) {
   const qty = 1;
   const { dispatch, setAlert, setMessage, setAlert_bg } = useContext(AppContext);
 
@@ -16,7 +16,7 @@ function Card({ item }) {
   return (
     <div className="sm:basis-[45%] lg:basis-[32.5%] border-2 m-1 p-2">
       <div className="w-full lg:h-80 sm:h-28 flex justify-center overflow-hidden overflow-clip">
-        <img className="h-full" src={`../src/assets/${item.image[0]}`} alt={item.name} />
+        <img className="h-full" src={`${url}${item.image[0]}`} alt={item.name} />
       </div>
 
       <div className="flex items-center">
