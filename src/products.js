0ce5a81d5c2b206f6d39f2fde576aee4e8893e6d
paul uwaifo <uwaifopaul1@gmail.com>
2024-07-08 15:35:03 +0100

    updated

--- src/products.js
+++ src/products.js
@@ -1,5 +1,3 @@
-
-
 export const products = [
   {
     id: "GGOEAFKA087495",
@@ -11,7 +9,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2822_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2822_480.jpg", "image2"],
     price: 98,
 
     inStock: 10,
@@ -26,7 +24,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2823_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2823_480.jpg", "image2"],
     price: 125,
 
     inStock: 10,
@@ -41,7 +39,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2824_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2824_480.jpg", "image2"],
     price: 100,
 
     inStock: 10,
@@ -56,7 +54,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/98025d05-ecfb-472e-ac4c-dfd8f88eb181_720.jpg", "image2"],
+    image: ["./assets/product_images/98025d05-ecfb-472e-ac4c-dfd8f88eb181_720.jpg", "image2"],
     price: 132,
 
     inStock: 10,
@@ -71,7 +69,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2826_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2826_480.jpg", "image2"],
     price: 132,
 
     inStock: 10,
@@ -86,7 +84,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2825_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2825_480.jpg", "image2"],
     price: 132,
 
     inStock: 10,
@@ -101,7 +99,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/img_2831_480.jpg", "image2"],
+    image: ["./assets/product_images/img_2831_480.jpg", "image2"],
     price: 132,
 
     inStock: 10,
@@ -116,7 +114,7 @@ export const products = [
 
     keywords:
       "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
-    image: ["../src/assets/product_images/08c20918-8355-4187-97c6-a995246386f7_720.jpg", "image2"],
+    image: ["./assets/product_images/08c20918-8355-4187-97c6-a995246386f7_720.jpg", "image2"],
     price: 132,
 
     inStock: 10,
